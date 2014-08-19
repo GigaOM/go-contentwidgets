5b1218364e6aa1553f7432e6000ded30d01ab618
Matthew Batchelder <borkweb@gmail.com>
2014-08-19 10:38:04 -0400

    Fixing a singleton bug and adding a bunch of stuff

---
+++ components/class-go-content-widgets-admin.php
@@ -0,0 +1,47 @@
+<?php
+
+class GO_Content_Widgets_Admin
+{
+	/**
+	 * constructor
+	 */
+	public function __construct()
+	{
+		add_action( 'current_screen', array( $this, 'current_screen' ) );
+	}//end __construct
+
+	/**
+	 * registers script and style resources
+	 */
+	private function register_resources()
+	{
+		wp_register_script(
+			go_content_widgets()->id_base . '-admin',
+			plugins_url( 'js/lib/go-content-widgets-admin.js', __FILE__ ),
+			array( 'jquery' ),
+			go_content_widgets()->script_config( 'version' ),
+			TRUE
+		);
+	}//end register_resources
+
+	/**
+	 * hooked to the current_screen action
+	 */
+	public function current_screen()
+	{
+		$screen = get_current_screen();
+		do_action( 'debug_robot', print_r( $screen, TRUE ) );
+
+		$this->register_resources();
+
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+	}//end current_screen
+
+	/**
+	 * hooked to the admin_enqueue_scripts action
+	 */
+	public function admin_enqueue_scripts()
+	{
+		wp_enqueue_script( go_content_widgets()->id_base . '-admin' );
+	}//end admin_enqueue_scripts
+}//end class
--- components/class-go-content-widgets.php
+++ components/class-go-content-widgets.php
@@ -2,13 +2,67 @@
 
 class GO_Content_Widgets
 {
+	public $id_base = 'go-content-widgets';
+	private $admin;
+	private $script_config;
+
+	/**
+	 * constructor
+	 */
+	public function __construct()
+	{
+		add_action( 'init', array( $this, 'init' ) );
+	}//end __construct
+
+	/**
+	 * hooked to the init action
+	 */
+	public function init()
+	{
+		if ( is_admin() )
+		{
+			$this->admin();
+		}//end if
+	}//end init
+
+	/**
+	 * script config
+	 */
+	public function script_config( $key = NULL )
+	{
+		if ( ! $this->script_config )
+		{
+			$this->script_config = apply_filters( 'go_config', array( 'version' => 1 ), 'go-script-version' );
+		}//end if
+
+		if ( $key )
+		{
+			return isset( $this->script_config[ $key ] ) ? $this->script_config[ $key ] : NULL;
+		}//end if
+
+		return $this->script_config;
+	}//end script_config
+
+	/**
+	 * admin object accessor method
+	 */
+	public function admin()
+	{
+		if ( ! $this->admin )
+		{
+			require_once __DIR__ . '/class-go-content-stats-admin.php';
+			$this->admin = new GO_Content_Stats_Admin;
+		}//end if
+
+		return $this->admin;
+	}//end admin
 }//end class
 
 function go_content_widgets()
 {
 	global $go_content_widgets;
 
-	if ( $go_content_widgets )
+	if ( ! $go_content_widgets )
 	{
 		$go_content_widgets = new GO_Content_Widgets;
 	}//end if
