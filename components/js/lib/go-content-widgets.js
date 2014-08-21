a91778152c82d9848fbd1b544f968bcfeefb0df4
Matthew Batchelder <borkweb@gmail.com>
2014-08-21 13:18:46 -0400

    more performance stuff

--- components/js/lib/go-content-widgets.js
+++ components/js/lib/go-content-widgets.js
@@ -16,7 +16,7 @@ if ( 'undefined' === typeof go_content_widgets ) {
 		go_content_widgets.last = go_content_widgets.current;
 	};
 
-	go_content_widgets.init = function( auto_inject ) {
+	go_content_widgets.init = function() {
 		go_content_widgets.log( 'begin init' );
 		this.shortest_widget_height = 10000;
 		this.tallest_widget_height = 0;
@@ -30,36 +30,33 @@ if ( 'undefined' === typeof go_content_widgets ) {
 			'h1,h2,h3,h4,h5,h6'
 		];
 
-		go_content_widgets.log( 'finding key elements' );
 		this.$body = $( '#body' ).find( '.post section.body.entry-content' );
 		this.$content = this.$body.find( '> div' );
 		this.$widgets = $( '#hidden-sidebar > div' );
 		this.$first_element = this.$content.find( ':first' );
 		this.$images = this.$content.find( 'img' );
-		go_content_widgets.log( 'finished finding key elements' );
 
-		go_content_widgets.log( 'finding image' );
 		this.$images.each( function() {
 			var $img = $( this );
 
 			if ( $img.attr( 'width' ) < $img.closest( '.entry-content' ).width() ) {
-				$img.css( 'height', $img.attr( 'height' ) + 'px' );
+				$img.css( 'height', $img.attr( 'height' ).concat( 'px' ) );
 			} else {
 				$img.css( 'height', 'auto' );
 			}//end else
 		});
-		go_content_widgets.log( 'completed finding image' );
 
 		go_content_widgets.log( 'collecting widgets' );
 		this.$widgets.each( function() {
-			var widget_id = $( this ).attr( 'id' );
+			var $widget = $( this );
+			var widget_id = $widget.attr( 'id' );
 
-			$( this ).addClass( 'layout-box-insert' ); // @todo, this may not be needed long term, but for now it makes the CSS easier
+			$widget.addClass( 'layout-box-insert' ); // @todo, this may not be needed long term, but for now it makes the CSS easier
 
 			var widget = {
 				name: widget_id,
-				$el: $( this ),
-				height: parseInt( $( this ).outerHeight( true ) * 0.9, 10 ),
+				$el: $widget,
+				height: parseInt( $widget.outerHeight( true ) * 0.9, 10 ),
 				location: 'right',
 				preferbottom: false
 			};
@@ -89,16 +86,14 @@ if ( 'undefined' === typeof go_content_widgets ) {
 			}//end if
 
 			if ( widget.location ) {
-				$( this ).addClass( 'layout-box-insert-' + widget.location );
+				$widget.addClass( 'layout-box-insert-'.concat( widget.location ) );
 			}//end if
 
 			go_content_widgets.insert.push( widget );
 		} );
 		go_content_widgets.log( 'finished collecting widgets' );
 
-		go_content_widgets.log( 'begin auto inject' );
 		this.auto_inject();
-		go_content_widgets.log( 'end auto inject' );
 		this.$content.find( '.layout-box-thing' ).remove();
 		$( '#body' ).addClass( 'rendered' );
 		go_content_widgets.current = Date.now();
@@ -110,11 +105,8 @@ if ( 'undefined' === typeof go_content_widgets ) {
 	 */
 	go_content_widgets.auto_inject = function() {
 		for ( var i = 0, length = go_content_widgets.insert.length; i < length; i++ ) {
-			go_content_widgets.log( 'begin calc' );
 			go_content_widgets.calc();
-			go_content_widgets.log( 'end calc/begin inject' );
 			go_content_widgets.inject_item( go_content_widgets.insert[ i ] );
-			go_content_widgets.log( 'end inject' );
 		}// end foreach
 	};
 
@@ -145,7 +137,8 @@ if ( 'undefined' === typeof go_content_widgets ) {
 	};
 
 	go_content_widgets.overlay = function( $el, start, height, type ) {
-		var $overlay = $( '<div class="layout-box-thing ' + type + '" style="top:' + start + 'px;height:' + height + 'px;"></div>' );
+		// for a lot of concatenations like this one, join is faster: http://moduscreate.com/javascript-performance-tips-tricks/
+		var $overlay = $( [ '<div class="layout-box-thing ', type, '" style="top:', start, 'px;height:', height, 'px;"></div>' ].join() );
 		if ( 'gap' === type ) {
 			$el.before( $overlay );
 		} else if( 'solo-gap' === type ) {
@@ -166,9 +159,8 @@ if ( 'undefined' === typeof go_content_widgets ) {
 	};
 
 	go_content_widgets.calc = function() {
-		go_content_widgets.log( 'begin reset' );
 		this.reset();
-		go_content_widgets.log( 'end reset/begin identify blackouts' );
+		go_content_widgets.log( 'begin identify blackouts' );
 		this.identify_blackouts();
 		go_content_widgets.log( 'end identify blackouts/begin identify gaps' );
 		this.identify_gaps();
@@ -176,6 +168,7 @@ if ( 'undefined' === typeof go_content_widgets ) {
 	};
 
 	go_content_widgets.identify_blackouts = function() {
+		go_content_widgets.log( 'before find :visible' );
 		// find top level blackouts
 		this.$content.find( '> *:visible:not(p):not(ol):not(ul):not(script):not(address)' ).each( function() {
 			var $el = $( this );
@@ -191,6 +184,7 @@ if ( 'undefined' === typeof go_content_widgets ) {
 			go_content_widgets.inventory.blackouts.push( attr );
 		});
 
+		go_content_widgets.log( 'after find :visible / before find children' );
 		// find child blackouts
 		this.$content.find( '> p *' ).each( function() {
 			var $el = $( this );
@@ -203,9 +197,12 @@ if ( 'undefined' === typeof go_content_widgets ) {
 			go_content_widgets.inventory.blackouts.push( attr );
 		});
 
+		go_content_widgets.log( 'after find children / before blackout overlay generation' );
+
 		// draw the blackout overlays
 		for ( var i = 0, length = this.inventory.blackouts.length; i < length; i++ ) {
 			var blackout = this.inventory.blackouts[ i ];
+
 			if ( blackout.is_child ) {
 				blackout.$overlay = this.overlay( blackout.$el.closest( 'p' ), blackout.start, blackout.height, 'blackout' );
 			}// end if
@@ -213,6 +210,7 @@ if ( 'undefined' === typeof go_content_widgets ) {
 				blackout.$overlay = this.overlay( blackout.$el, blackout.start, blackout.height, 'blackout' );
 			}// end else
 		}//end for
+		go_content_widgets.log( 'after blackout overlay generation' );
 
 		this.inventory.blackouts.sort( this.sort_by_start );
 	};
@@ -231,6 +229,8 @@ if ( 'undefined' === typeof go_content_widgets ) {
 		var gap;
 		var $overlay;
 		var i;
+		var gap_height;
+		var length;
 
 		if ( 0 === this.inventory.blackouts.length ) {
 			$overlay = this.overlay( this.$content, start, this.$content.outerHeight(), 'solo-gap' );
@@ -243,11 +243,11 @@ if ( 'undefined' === typeof go_content_widgets ) {
 		else {
 
 			var previous_blackout = null;
-			for ( var i = 0, length = this.inventory.blackouts.length; i < length; i++ ) {
+			for ( i = 0, length = this.inventory.blackouts.length; i < length; i++ ) {
 				var blackout = this.inventory.blackouts[ i ];
 
 				if ( blackout.start > start ) {
-					var gap_height = blackout.start - start;
+					gap_height = blackout.start - start;
 					if ( 0 === gap_height ) {
 						continue;
 					}//end if
@@ -346,37 +346,9 @@ if ( 'undefined' === typeof go_content_widgets ) {
 		$element.before( item.$el );
 	};
 
-	var check = false, is_relative = true;
-
-	go_content_widgets.element_from_point = function( x, y ) {
-		if ( ! document.elementFromPoint ) {
-			return null;
-		}//end if
-
-		if ( ! check ) {
-			var sl;
-
-			if ( ( sl = $( document ).scrollTop() ) > 0 ) {
-				is_relative = ( document.elementFromPoint( 0, sl + $( window ).height() -1 ) === null );
-			} else if ( ( sl = $( document ).scrollLeft() ) > 0 ) {
-				is_relative = ( document.elementFromPoint( sl + $( window ).width() -1, 0 ) === null );
-			}//end else if
-
-			check = ( sl > 0 );
-		}//end if
-
-		if ( ! is_relative ) {
-			x += $( document ).scrollLeft();
-			y += $( document ).scrollTop();
-		}//end if
-
-		return document.elementFromPoint( x, y );
-	};
-
 	$( function() {
 		go_content_widgets.start = Date.now();
 		go_content_widgets.last = go_content_widgets.start;
-		go_content_widgets.log( 'before calling init' );
 		go_content_widgets.init();
 	});
 })( jQuery );
